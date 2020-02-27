df9966377e85debc46f67d452a1cc2382ee8205c
Reynold <reydev.sourcecoder@gmail.com>
2020-02-27 17:35:20 +0800

    added products service and implemented dependency injection

--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector : 'ba-products',
@@ -25,61 +26,11 @@ export class ProductListComponent
             this.performFilter(this.listFilter) : this.products;
     }
 
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2019",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2019",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        },
-        {
-            "productId": 8,
-            "productName": "Saw",
-            "productCode": "TBX-0022",
-            "releaseDate": "May 15, 2019",
-            "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-            "starRating": 3.7,
-            "imageUrl": "assets/images/saw.png"
-        },
-        {
-            "productId": 10,
-            "productName": "Video Game Controller",
-            "productCode": "GMG-0042",
-            "releaseDate": "October 15, 2018",
-            "description": "Standard two-button video game controller",
-            "price": 35.95,
-            "starRating": 4.6,
-            "imageUrl": "assets/images/xbox-controller.png"
-        }
-    ];
+    products: IProduct[];
     filteredProducts: IProduct[];
 
-    constructor() {
+    constructor(private _productService: ProductService) {
+        this.products = _productService.getProducts();
         this.filteredProducts = this.products;
         this.listFilter = 'cart';
     }
