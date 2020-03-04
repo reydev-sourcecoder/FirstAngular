2614b1c63af31f8bc76ffed3ef1d24a2f933053d
Reynold <reydev.sourcecoder@gmail.com>
2020-03-04 18:42:24 +0800

    Created products module for code organization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Router } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './product/product-detail.component';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailGuard } from './product/product-detail.guard';
+import { ProductModule } from './product/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'product', component: ProductListComponent },
-      { path: 'product/:id', 
-        component: ProductDetailComponent, 
-        canActivate:[ProductDetailGuard] 
-      },
       { path: 'home', component: HomeComponent },
-      { path: '', redirectTo: 'product', pathMatch: 'full'},
-      // { path:'**', component: PageNotFoundComponent}
-    ])
+      { path: '', redirectTo: 'home', pathMatch: 'full'}
+    ]),
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
